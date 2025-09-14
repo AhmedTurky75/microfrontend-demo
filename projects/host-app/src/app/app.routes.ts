84cd3b59557646bbcd704d04320c94262fc62ba4
Ahmed Turky <Ahmedturky75@gmail.com>
2025-09-14 23:39:25 +0300

    dynamic dynamic federation

--- projects/host-app/src/app/app.routes.ts
+++ projects/host-app/src/app/app.routes.ts
@@ -2,33 +2,33 @@ import { Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/module-federation';
 
 export const routes: Routes = [
-  {
-    path: 'feature',
-    loadChildren: () =>
-      loadRemoteModule({
-        type: 'manifest',
-        remoteName: 'mfe-one',
-        exposedModule: './FeatureModule'
-      }).then(m => m.FeatureModule)
-  },
-  {
-    path: 'feature/first',
-    loadChildren: () =>
-      loadRemoteModule({
-        type: 'manifest',
-        remoteName: 'mfe-one',
-        exposedModule: './FeatureModule'
-      }).then(m => m.FeatureModule)
-  },
-  {
-    path: 'feature/second',
-    loadChildren: () =>
-      loadRemoteModule({
-        type: 'manifest',
-        remoteName: 'mfe-one',
-        exposedModule: './FeatureModule'
-      }).then(m => m.FeatureModule)
-  }
+  // {
+  //   path: 'feature',
+  //   loadChildren: () =>
+  //     loadRemoteModule({
+  //       type: 'manifest',
+  //       remoteName: 'mfe-one',
+  //       exposedModule: './FeatureModule'
+  //     }).then(m => m.FeatureModule)
+  // },
+  // {
+  //   path: 'feature/first',
+  //   loadChildren: () =>
+  //     loadRemoteModule({
+  //       type: 'manifest',
+  //       remoteName: 'mfe-one',
+  //       exposedModule: './FeatureModule'
+  //     }).then(m => m.FeatureModule)
+  // },
+  // {
+  //   path: 'feature/second',
+  //   loadChildren: () =>
+  //     loadRemoteModule({
+  //       type: 'manifest',
+  //       remoteName: 'mfe-one',
+  //       exposedModule: './FeatureModule'
+  //     }).then(m => m.FeatureModule)
+  // }
     // canActivate: [FeatureAccessGuard]
     // },
 ];
--- projects/host-app/src/app/app.ts
+++ projects/host-app/src/app/app.ts
@@ -1,5 +1,8 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { getManifest } from '@angular-architects/module-federation';
+import { CustomRemoteConfig, CustomManifest } from './utils/config';
+import { buildRoutes } from './utils/routes';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +10,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('host-app');
+// export class App {
+//   protected readonly title = signal('host-app');
+// }
+
+export class App implements OnInit  {
+
+  remotes: CustomRemoteConfig[] = [];
+
+  constructor(
+    private router: Router) {
+  }
+
+  async ngOnInit(): Promise<void> {
+    const manifest = getManifest<CustomManifest>();
+
+    // Hint: Move this to an APP_INITIALIZER 
+    //  to avoid issues with deep linking
+    const routes = buildRoutes(manifest);
+    this.router.resetConfig(routes);
+
+    this.remotes = Object.values(manifest);
+  }
 }
