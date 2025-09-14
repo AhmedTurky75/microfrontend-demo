626c17ecd27880bb5578a8ffa94363cf6906492b
Ahmed Turky <Ahmedturky75@gmail.com>
2025-09-14 23:39:13 +0300

    dynamic federation

--- projects/host-app/src/bootstrap.ts
+++ projects/host-app/src/bootstrap.ts
@@ -1,17 +1,26 @@
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { App } from './app/app';
+// import { provideRouter, Router } from '@angular/router';
+// import { provideHttpClient } from '@angular/common/http';
+// import { routes } from './app/app.routes';
+// import { DynamicRoutesService } from './Servicecs/dynamic-routes.service';
+// bootstrapApplication(App, {
+//   providers: [
+//     provideHttpClient(),
+//     provideRouter(routes),
+//   ],
+// })
+// .then(appRef => {
+//   const injector = appRef.injector;
+//   const dynamicRoutes = injector.get(DynamicRoutesService);
+//   dynamicRoutes.init();
+// });
+
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { provideRouter, Router } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { routes } from './app/app.routes';
-import { DynamicRoutesService } from './Servicecs/dynamic-routes.service';
-bootstrapApplication(App, {
-  providers: [
-    provideHttpClient(),
-    provideRouter(routes),
-  ],
-}).then(appRef => {
-  const injector = appRef.injector;
-  const dynamicRoutes = injector.get(DynamicRoutesService);
-  dynamicRoutes.init();
-});
+
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
--- projects/host-app/src/main.ts
+++ projects/host-app/src/main.ts
@@ -1,9 +1,9 @@
-// import { loadManifest } from '@angular-architects/module-federation';
+import { loadManifest } from '@angular-architects/module-federation';
 
-// loadManifest('assets/mf.manifest.json')
-// 	.catch((err: Error) => console.error('Error loading remote entries', err))
-// 	.then(() => import('./bootstrap'))
-// 	.catch((err: Error) => console.error(err));
+loadManifest('assets/mf.manifest.json')
+	.catch((err: Error) => console.error('Error loading remote entries', err))
+	.then(() => import('./bootstrap'))
+	.catch((err: Error) => console.error(err));
 
-import('./bootstrap')
-	.catch(err => console.error(err));
+// import('./bootstrap')
+// 	.catch(err => console.error(err));
